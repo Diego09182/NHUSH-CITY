c39b74905b2278fadb0066cf953e500089b4d672
張皓明 <63291576+Diego09182@users.noreply.github.com>
2021-10-10 18:00:31 +0800

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -17,31 +17,29 @@ const About = {
 	template:`
 	<div class="row">
 	
-			<div id="modal1" class="modal">
-				<div class="modal-content">
-					<form action="checkpwd.php" method="post" name="Form">
-						<h4 class="center-align">系統登入</h4>
-						<div class="input-field col s12">
-							<i class="material-icons prefix">account_circle</i>
-							<input name="account" id="icon_prefix" type="text" class="validate" size="15">
-							<label for="icon_prefix">帳號</label>
-						</div>
-						<div class="input-field col s12">
-							<i class="material-icons prefix">https</i>
-							<input name="password" id="password" type="password" class="validate" size="15">
-							<label for="icon_telephone">密碼</label>
-						</div>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<a class=" modal-action modal-close waves-effect waves-green btn-flat">關閉</a>
-					<a class="waves-effect waves-light btn" onClick="check_login()">登入</a>
-				</div>
-			</div>
+		  <div id="modal1" class="modal">
+		  	<div class="modal-content">
+		  		<form action="checkpwd.php" method="post" name="Form">
+		  			<h4 class="center-align">系統登入</h4>
+		  			<div class="input-field col s12 m6">
+		  				<i class="material-icons prefix">account_circle</i>
+		  				<input name="account" id="icon_prefix" type="text" class="validate" size="15">
+		  				<label for="icon_prefix">帳號</label>
+		  			</div>
+		  			<div class="input-field col s12 m6">
+		  				<i class="material-icons prefix">https</i>
+		  				<input name="password" id="password" type="password" class="validate" size="15">
+		  				<label for="icon_telephone">密碼</label>
+		  			</div>
+		  		</form>
+		  	</div>
+		  	<div class="modal-footer">
+		  		<a class=" modal-action modal-close waves-effect waves-green btn-flat">關閉</a>
+		  		<a class="waves-effect waves-light btn" onClick="check_login()">登入</a>
+		  	</div>
+		  </div>
 				
 		  <div class="row">	
-
-			
 			<div id="modal2" class="modal">
 				<div class="modal-content">
 					<form action="addmember.php" method="post" name="myForm">
@@ -112,29 +110,27 @@ const About = {
 				</div>
 			</div>
 	
-	<div class="container">
 		<div class="container">
 			<h3 class="center-align">系統開發起始</h3>
-				<div class="row">
-					<div class="col s12 m12">
-					  <div class="card brown">
-						<div class="card-content white-text">
-							<span class="card-title"><I>"要做出適合學生交流、發表學習成果的平台"</I>
-							</span>
-							<br>
-							<span class="card-content">
-							<I class="right"></I>
-							</span>
-						</div>
-						<div class="card-action">
-							<p class="left">讚:</p>
-							<a class="btn-floating halfway-fab waves-effect waves-light brown right"><i class="tooltipped" data-delay="50" data-tooltip="只能點讚ww"><i class="material-icons">thumb_up</i></i></a>
-						</div>
+			<div class="row">
+				<div class="col s12 m12">
+				  <div class="card brown">
+					<div class="card-content white-text">
+						<span class="card-title"><I>"要做出適合學生交流、發表學習成果的平台"</I>
+						</span>
 						<br>
-						<br>
-					  </div>
+						<span class="card-content">
+						<I class="right"></I>
+						</span>
 					</div>
+					<div class="card-action">
+						<a class="btn-floating halfway-fab waves-effect waves-light brown right"><i class="tooltipped" data-delay="50" data-tooltip="只能點讚ww"><i class="material-icons">thumb_up</i></i></a>
+					</div>
+					<br>
+					<br>
+				  </div>
 				</div>
+			</div>
 		</div>
 		
 		<div class="container">
@@ -169,18 +165,16 @@ const About = {
 						  <ul class="collapsible" data-collapsible="accordion">
 						    <li>
 						      <div class="collapsible-header">球隊</div>
-						      <div class="collapsible-body"><p>大家都很棒歐。(官腔)</p></div>
+						      <div class="collapsible-body"><p>大家都很棒歐。</p></div>
 						    </li>
 						    <li>
 						      <div class="collapsible-header">社團</div>
-						      <div class="collapsible-body"><p>大家都很棒歐。(官腔)</p></div>
+						      <div class="collapsible-body"><p>大家都很棒歐。</p></div>
 						    </li>
 							<li>
 							  <div class="collapsible-header">學生自治</div>
 							  <div class="collapsible-body">
-							  <p>協助試辦及草擬高級中等學校學生自治組織補助社團經費辦法，提供北市各校參考，但成效不彰，僅小田園服務隊申請補助</p>
-							  <p>於106、107、108學年度，南湖班聯會成員皆有因酒後行為，導致校內外觀感不佳之事件。</p>
-							  <p>在108年度，選舉下一屆班聯會主席時，校方擅自更改為電子投票，且該系統遭到惡意攻擊，後經修正有效投票率僅有22.03%，三次選舉後確立下年度之新任班聯會正副主席</p>
+								<p>可以選擇班聯會代表，每個代表都很認真爭取學生權益。</p>
 							  </div>
 							</li>
 						  </ul>
@@ -206,10 +200,10 @@ const About = {
 						            <td>社團經費缺少</td>
 						          </tr>
 						          <tr>
-						            <td>校本必修超無聊</td>
+						            <td>校本必修內容較無趣</td>
 						          </tr>
 						          <tr>
-						            <td>秋/冬天的地板，下雨時超濕，比你阿公還持久的那種</td>
+						            <td>秋/冬天的地板，會比較濕</td>
 						          </tr>
 						        </tbody>
 						      </table>
@@ -237,13 +231,13 @@ const Disclaimer = {
 			<div id="modal1" class="modal">
 				<div class="modal-content">
 					<form action="checkpwd.php" method="post" name="Form">
-					  <h4 class="center-align">系統登入</h4>
-						<div class="input-field col s6">
+						<h4 class="center-align">系統登入</h4>
+						<div class="input-field col s12 m6">
 							<i class="material-icons prefix">account_circle</i>
 							<input name="account" id="icon_prefix" type="text" class="validate" size="15">
 							<label for="icon_prefix">帳號</label>
 						</div>
-						<div class="input-field col s6">
+						<div class="input-field col s12 m6">
 							<i class="material-icons prefix">https</i>
 							<input name="password" id="password" type="password" class="validate" size="15">
 							<label for="icon_telephone">密碼</label>
@@ -325,80 +319,79 @@ const Disclaimer = {
 					<a class="waves-effect waves-light btn" onClick="check_data()">註冊</a>
 				</div>
 			</div>
-		
-		<br>
+			
+			<br>
+			
+			<div class="container">
 				<div class="container">
-					<section id="plan" class="tm-section-pad-top">
-						  <div class="container">
-							<div class="center-align">
-								<h3 class="tm-text-primary tm-section-title mb-4">社群規則</h3>
+					<div class="center-align">
+						<h3 class="tm-text-primary tm-section-title mb-4">社群規則</h3>
+					</div>
+					  <div class="col s12 m12">
+						<div class="card horizontal">
+						  <div class="card-stacked">
+							<div class="card-content">
+								<h5>
+								  <blockquote>
+											<table>
+											  <thead>
+												<tr>
+												  <th data-field="id">規則</th>
+												  <th data-field="name">處罰內容</th>
+												</tr>
+											  </thead>
+											  <tbody>
+												<tr>
+												  <td>禁止侵犯他人著作權</td>
+												  <td>封鎖帳號</td>
+												</tr>
+												<tr>
+												  <td>禁止任何犯罪行為</td>
+												  <td>封鎖帳號</td>
+												</tr>
+												<tr>
+												  <td>禁止發送垃圾訊息</td>
+												  <td>封鎖帳號</td>
+												</tr>
+												<tr>
+												  <td>禁止宣傳未授權的任何產品及服務</td>
+												  <td>封鎖帳號</td>
+												</tr>
+											  </tbody>
+											</table>
+								  </blockquote>
+								</h5>
 							</div>
-							  <div class="col s12 m12">
-								<div class="card horizontal">
-								  <div class="card-stacked">
-									<div class="card-content">
-										<h5>
-										  <blockquote>
-											        <table>
-											          <thead>
-											            <tr>
-														  <th data-field="id">規則</th>
-														  <th data-field="name">處罰內容</th>
-											            </tr>
-											          </thead>
-											          <tbody>
-											            <tr>
-											              <td>禁止侵犯他人著作權</td>
-											              <td>封鎖帳號</td>
-											            </tr>
-											            <tr>
-											              <td>禁止任何犯罪行為</td>
-											              <td>封鎖帳號</td>
-											            </tr>
-											            <tr>
-											              <td>禁止發送垃圾訊息</td>
-											              <td>封鎖帳號</td>
-											            </tr>
-														<tr>
-														  <td>禁止宣傳未授權的任何產品及服務</td>
-														  <td>封鎖帳號</td>
-														</tr>
-											          </tbody>
-											        </table>
-										  </blockquote>
-										</h5>
-									</div>
-								  </div>
-								</div>
-							  </div>
-					</section>
+						  </div>
+						</div>
+					  </div>
 				</div>
-				
+			</div>
+			<br>
+			
+			<div class="container">
 				<div class="container">
-					<section id="plan" class="tm-section-pad-top">
-						  <div class="container">
-							<div class="center-align">
-								<h3 class="tm-text-primary tm-section-title mb-4">免責聲明</h3>
+						<div class="center-align">
+							<h3 class="tm-text-primary tm-section-title mb-4">免責聲明</h3>
+						</div>
+					  <div class="col s12 m12">
+						<div class="card horizontal">
+						  <div class="card-stacked">
+							<div class="card-content">
+								<h4>
+									<blockquote>
+										開發者及運營者不為使用本站的用戶，負起其任何行為的法律責任。
+									</blockquote>
+									<blockquote>
+										對使用或連結本網頁而引致任何損害或其他無形損失，本網站不承擔任何直接、間接、附帶、特別、衍生性或懲罰性賠償。
+									</blockquote>
+								</h4>
 							</div>
-							  <div class="col s12 m12">
-								<div class="card horizontal">
-								  <div class="card-stacked">
-									<div class="card-content">
-										<h4>
-											<blockquote>
-												開發者及運營者不為使用本站的用戶，負起其任何行為的法律責任
-											</blockquote>
-											<blockquote>
-												對使用或連結本網頁而引致任何損害或其他無形損失，本網站不承擔任何直接、間接、附帶、特別、衍生性或懲罰性賠償。
-											</blockquote>
-										</h4>
-									</div>
-								  </div>
-								</div>
-							  </div>
-					</section>
+						  </div>
+						</div>
+					  </div>
 				</div>
-		
+			</div>
 			
 	</div>` 
 }
@@ -407,30 +400,30 @@ const Home = {
 	template:` 
 	<div class="container">
 		<div class="section" id="features">
-			
-		  <!--   Icon Section   -->
+
 		  <div class="row">	
-				<div id="modal1" class="modal">
-					<div class="modal-content">
-						<form action="checkpwd.php" method="post" name="Form">
-						  <h4 class="center-align">系統登入</h4>
-							<div class="input-field col s6">
-								<i class="material-icons prefix">account_circle</i>
-								<input name="account" id="icon_prefix" type="text" class="validate" size="15">
-								<label for="icon_prefix">帳號</label>
-							</div>
-							<div class="input-field col s6">
-								<i class="material-icons prefix">https</i>
-								<input name="password" id="password" type="password" class="validate" size="15">
-								<label for="icon_telephone">密碼</label>
-							</div>
-						</form>
-					</div>
-					<div class="modal-footer">
-						<a class=" modal-action modal-close waves-effect waves-green btn-flat">關閉</a>
-						<a class="waves-effect waves-light btn" onClick="check_login()">登入</a>
-					</div>
+		  
+			<div id="modal1" class="modal">
+				<div class="modal-content">
+					<form action="checkpwd.php" method="post" name="Form">
+						<h4 class="center-align">系統登入</h4>
+						<div class="input-field col s12 m6">
+							<i class="material-icons prefix">account_circle</i>
+							<input name="account" id="icon_prefix" type="text" class="validate" size="15">
+							<label for="icon_prefix">帳號</label>
+						</div>
+						<div class="input-field col s12 m6">
+							<i class="material-icons prefix">https</i>
+							<input name="password" id="password" type="password" class="validate" size="15">
+							<label for="icon_telephone">密碼</label>
+						</div>
+					</form>
 				</div>
+				<div class="modal-footer">
+					<a class=" modal-action modal-close waves-effect waves-green btn-flat">關閉</a>
+					<a class="waves-effect waves-light btn" onClick="check_login()">登入</a>
+				</div>
+			</div>
 			
 			<div id="modal2" class="modal">
 				<div class="modal-content">
@@ -438,29 +431,29 @@ const Home = {
 					  <h4 class="center-align">註冊帳號</h4>
 						<div class="input-field col s12 m6">
 							<i class="material-icons prefix">account_circle</i>
-							<input name="name" id="icon_prefix" type="text" class="validate">
+							<input name="name" id="name" type="text" class="validate">
 							<label for="icon_prefix">學生姓名</label>
 						</div>
 						<div class="input-field col s12 m6">
 							<i class="material-icons prefix">https</i>
-							<input name="student_ID" id="icon_telephone" type="text" class="validate" size="15">
+							<input name="student_ID" id="student_ID" type="text" class="validate" size="15">
 							<label for="icon_telephone">學號</label>
 						</div>
 						<div class="input-field col s4">
-							<input name="year" id="icon_prefix" type="text" class="validate" size="2">
+							<input name="year" id="year" type="text" class="validate" size="2">
 							<label for="icon_prefix">年(民國)</label>
 						</div>
 						<div class="input-field col s4">
-							<input name="month" id="icon_prefix" type="text" class="validate" size="2">
+							<input name="month" id="month" type="text" class="validate" size="2">
 							<label for="icon_prefix">月</label>
 						</div>
 						<div class="input-field col s4">
-							<input name="day" id="icon_prefix" type="text" class="validate" size="2">
+							<input name="day" id="day" type="text" class="validate" size="2">
 							<label for="icon_prefix">日</label>
 						</div>
 						<div class="input-field col s12 m6">
 							<i class="material-icons prefix">account_circle</i>
-							<input name="account" id="icon_prefix" type="text" class="validate" size="15">
+							<input name="account" id="account" type="text" class="validate" size="15">
 							<label for="icon_prefix">註冊帳號</label>
 						</div>
 						<br> 
@@ -481,17 +474,17 @@ const Home = {
 						</div>
 						<div class="input-field col s12">
 							<i class="material-icons prefix">account_circle</i>
-							<input name="interest" id="cellphone" type="text" size="50" class="validate">
+							<input name="interest" id="interest" type="text" size="50" class="validate">
 							<label for="telephone" data-error="wrong" data-success="right">個人興趣</label>
 						</div>
 						<div class="input-field col s12">
 							<i class="material-icons prefix">account_circle</i>
-							<input name="url" id="cellphone" type="text" size="50" class="validate">
+							<input name="url" id="url" type="text" size="50" class="validate">
 							<label for="telephone" data-error="wrong" data-success="right">個人網站</label>
 						</div>
 						<div class="input-field col s12">
 							<i class="material-icons prefix">account_circle</i>
-							<input name="comment" id="introduction" type="text" class="validate">
+							<input name="comment" id="comment" type="text" class="validate">
 							<label for="comment" data-error="wrong" data-success="right">個人簡介</label>
 						</div>
 					</form>
@@ -624,47 +617,6 @@ const Home = {
 				<br>
 			  </div>            	
 		</section>
-		</div>
-		
-		<div class="row">
-		<div class="center-align">
-			<h3 class="tm-text-primary tm-section-title mb-4">系統更新</h3>
-		</div>
-		<div class="carousel">
-		    <a class="carousel-item" href="#one!">	          
-				<div class="card blue-grey darken-1">
-					<div class="card-content white-text">
-						<span class="card-title">頁面更新</span>
-						<p>關於網站與網站聲明</p>
-					</div>
-					<br>
-					<br>
-				</div>
-			</a>
-		    <a class="carousel-item" href="#one!">
-				<div class="">
-				  <div class="card blue-grey darken-1">
-					<div class="card-content white-text">
-						<span class="card-title">系統更新</span>
-						<p>登入系統部分完成</p>
-					</div>
-					<br>
-					<br>
-				</div>
-		    </a>
-		    <a class="carousel-item" href="#one!">
-				<div class="">
-				  <div class="card blue-grey darken-1">
-					<div class="card-content white-text">
-						<span class="card-title">系統更新</span>
-						<p>投票系統部分完成</p>
-					</div>
-					<br>
-					<br>
-				  </div>
-				</div>
-		    </a>
-		  </div>
 		</div>
 		
 		  </div>
@@ -833,23 +785,6 @@ const router = new VueRouter({
 					$('.carousel').carousel();
 					$('.slider').slider({full_width: true});
 					$('select').material_select();
-				},
-				increasefontsize() {
-				    this.fontStyle['font-size'] = `${this.fontsizesetting++}px`
-				},
-				decreasefontsize() {
-				    this.fontStyle['font-size'] = `${this.fontsizesetting--}px`
 				}
 			}
         })
-
-
-new Vue({
-		    el: '#app',
-		    data: {
-				about:'We are students of Nanhu High School and we love Computer Science and Information Engineering.',
-				copyright:'This website is completed by our students and teachers.',
-		    },
-		    methods: {
-		    }
-		})
