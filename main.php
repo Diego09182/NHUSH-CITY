eb1b8dbb863b3c1ac150f0fa68fe19c2f08eecf5
張皓明 <63291576+Diego09182@users.noreply.github.com>
2022-02-06 11:58:54 +0800

    Add files via upload

--- main.php
+++ main.php
@@ -40,7 +40,7 @@
 </head>
 <body>
 
-<div id='app'>	
+<div id="app">	
 	<nav class="light lighten-1 brown" role="navigation">
 	  <div class="nav-wrapper container"><a id="logo-container" href="main.php" class="brand-logo center">NHUSH-CITY</a>
 			<ul class="left hide-on-med-and-down">
@@ -96,32 +96,203 @@
 	  </div>
 	</nav>
     
-	<div class="section no-pad-bot" id="index-banner">
-	  <div class="container">
-	    <br><br>
-		<h1 class="center header-text animate__animated animate__fadeIn" id="index-title1" >南湖高中</h1>
-		<div class="row center">
-			<h5 class="header col s12 light animate__animated animate__fadeIn" id="index-title2">An exclusive community for Nanhu High School</h5>
-		</div>
-	    <br><br>
-	  </div>
-	</div>
+	<banner></banner>
 	
 	<br>
 	
-	<div class="fixed-action-btn horizontal click-to-toggle">
-	    <a class="btn-floating btn-large brown">
-			<i class="material-icons">menu</i>
-	    </a>
-	    <ul>
-			<li><a href="modify.php" class="btn-floating btn waves-effect waves-light green"><i class="tooltipped" data-position="top" data-tooltip="修改資料"><i class="material-icons">perm_identity</i></i></a></li>
-			<li><a href="myhome.php" class="btn-floating btn waves-effect waves-light blue"><i class="tooltipped" data-position="top" data-tooltip="我的小屋"><i class="material-icons">view_quilt</i></i></a></li>
-		</ul>
-	</div>
+	<tool></tool>
 	
 	<div class="container">
-
-		<div class="row">		
+		<features></features>	
+		<serve></serve>			
+	</div>
+	
+	<div class="center-align">
+		<h3>系統更新</h3>
+	</div>
+	
+	<div class="container">	
+		<?php
+			
+			//指定每頁顯示幾筆記錄
+			$records_per_page = 3;
+							
+			//取得要顯示第幾頁的記錄
+			if (isset($_GET["page"]))
+			  $page = $_GET["page"];
+			else
+			  $page = 1;
+									
+			//執行SQL查詢
+			$sql = "SELECT id, author, tag, subject, content, date FROM systemupdate ORDER BY date DESC";
+			$result = execute_sql($link, "news", $sql);
+								
+			//取得記錄數 
+			$total_records = mysqli_num_rows($result);
+							
+			//計算總頁數
+			$total_pages = ceil($total_records / $records_per_page);
+							
+			//計算本頁第一筆記錄的序號
+			$started_record = $records_per_page * ($page - 1);
+							
+			//將記錄指標移至本頁第一筆記錄的序號
+			mysqli_data_seek($result, $started_record);
+			
+			$j = 1;
+			
+			echo"<div class='row'>";
+				while ($row = mysqli_fetch_assoc($result) and $j <= $records_per_page)
+				{				
+					echo"<div class='center-align'>";
+						echo"<div class='col s4 m4'>";
+							echo"<div class='card blue-grey darken-1'>";
+								echo"<div class='card-content white-text'>";
+									echo"<span class='card-title'>".$row["subject"]."</span>";
+									echo"<br>";
+									echo"<p class='chip brown left'>".$row["tag"]."</p>";
+									echo"<br><br>";
+									echo"<h5 class=''>".$row["content"]."</h5>";
+									echo"<br>";
+									echo"<span class='author right'>發文者:".$row["author"]."</span>";
+									echo"<br>";
+									echo"<p class='right'>更新時間:".$row["date"]."</p>";
+								echo"</div>";
+								echo"<br>";
+							echo"</div>";
+						echo"</div>";
+					echo"</div>";
+					
+					$j++;
+				}
+			echo"</div>";
+			
+			//產生導覽列
+			echo"<ul class='pagination center'>";
+						
+			if ($page > 1)
+						echo "<li class='waves-effect'><a href='main.php?page=". ($page - 1) . "'><i class='material-icons'>chevron_left</i></a></li>";
+								
+			for ($i = 1; $i <= $total_pages; $i++)
+			{
+			  if ($i == $page)
+				echo "<li class='waves-effect'><a href='main.php?page=$i'>$i</a></li>";
+			  else
+				echo"<li class='waves-effect'><a href='main.php?page=$i'>$i</a></li>";
+			}
+							
+			if ($page < $total_pages)
+						echo"<li class='waves-effect'><a href='review.php?page=". ($page + 1) . "'><i class='material-icons'>chevron_right</i></a></li>";
+			echo "</p>";
+			
+			echo"</ul>";
+			
+		?>
+	</div>	
+	
+	<slogan></slogan>
+	
+	<?php
+	
+		//執行SQL查詢
+		$sql = "SELECT id,billboard,date FROM bulletin_board ORDER BY date DESC";
+		$result = execute_sql($link, "news", $sql);
+		$row = mysqli_fetch_assoc($result);
+		
+	?>
+	
+	<div class="container">
+		<div class="container">
+			<div class="center-align">
+				<h3 class="tm-text-primary tm-section-title mb-4">公佈欄</h3>
+			</div>
+			<div class="col s6 m6">
+				<div class="card horizontal small">
+					<div class="card-stacked">
+						<div class="card-content">
+							<h4>
+								<blockquote>
+									<?php  echo $row["billboard"]  ?>
+								</blockquote>
+							</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+	<br><br>
+	
+	<contact></contact>
+	
+	<br><br>
+	
+	<footers></footers>	
+	
+</div>
+<!--  Scripts-->
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.js" integrity="sha256-kRbW+SRRXPogeps8ZQcw2PooWEDPIjVQmN1ocWVQHRY=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v6.0"></script>
+<script src="js/init.js"></script>
+<script type="text/javascript">
+	
+	Vue.component('slogan', {
+	  template:  
+		`<div id="slogan">
+			<div class="row">
+				<div class="center-align col m12">
+					<br>
+					<h2 class="center-align">NHUSH CITY</h2>
+					<h5 class="center-align">
+					你能想像一個專屬於我們的社區嗎? 你可以在這裡分享對於學校與學習相關的任何問題。
+					<br>
+					<br>
+					我們會不斷精進此系統，請提供任何意見給開發團隊。
+					</h5>
+					<br>			
+					<br>
+				</div>
+			</div>		  
+		</div>`
+	})
+	
+	Vue.component('tool', {
+	  template:  
+		`<div class="fixed-action-btn horizontal click-to-toggle">
+			<a class="btn-floating btn-large brown">
+				<i class="material-icons">menu</i>
+			</a>
+			<ul>
+				<li><a href="modify.php" class="btn-floating btn waves-effect waves-light green"><i class="tooltipped" data-position="top" data-tooltip="修改資料"><i class="material-icons">perm_identity</i></i></a></li>
+				<li><a href="myhome.php" class="btn-floating btn waves-effect waves-light blue"><i class="tooltipped" data-position="top" data-tooltip="我的小屋"><i class="material-icons">view_quilt</i></i></a></li>
+			</ul>
+		</div>`
+	})
+	
+	Vue.component('banner', {
+	  template:  
+		`<div class="section no-pad-bot" id="index-banner">
+			<div class="container">
+				<br><br>
+					<h1 class="center header-text animate__animated animate__fadeIn" id="index-title1" >南湖高中</h1>
+				<div class="row center">
+					<h5 class="header col s12 light" id="index-title2">An exclusive community for Nanhu High School</h5>
+				</div>
+				<br><br>
+			</div>
+		</div>`
+	})
+	
+	Vue.component('features', {
+	  template:  
+		`<div class="row">		
 			<div class="container">	
 				<h3 class="center-align">主要功能</h3>
 				<br>
@@ -194,9 +365,12 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		
-		<div class="row">
+		</div>`
+	})
+	
+	Vue.component('serve', {
+	  template:  
+		`<div class="row">
 			<div class="container">	
 				<h3 class="center-align">主要服務</h3>
 				<br>
@@ -206,16 +380,14 @@
 					<h5 class="center">提供學習資料</h5>
 					<p class="light center">提供優良的學習歷程檔案範例</p>
 				  </div>
-				</div>
-		
+				</div>		
 				<div class="col s12 m4">
 				  <div class="icon-block">
 					<h2 class="center light-brown-text"><i class="animate__animated animate__heartBeat material-icons">comment</i></h2>
 					<h5 class="center">用戶經驗分享</h5>
 					<p class="light center">可以進行成果發表與討論分享</p>
 				  </div>
-				</div>
-		
+				</div>	
 				<div class="col s12 m4">
 				  <div class="icon-block">
 					<h2 class="center light-brown-text"><i class="animate__animated animate__heartBeat material-icons">assessment</i></h2>
@@ -224,200 +396,70 @@
 				  </div>
 				</div>
 			</div>
-		</div>
-		
-	</div>
+		</div>`
+	})
 	
-	<div class="center-align">
-		<h3 class="tm-text-primary tm-section-title mb-4">系統更新</h3>
-	</div>
-	
-	<div class="container">	
-	
-	<?php
-		
-		//指定每頁顯示幾筆記錄
-		$records_per_page = 3;
-						
-		//取得要顯示第幾頁的記錄
-		if (isset($_GET["page"]))
-		  $page = $_GET["page"];
-		else
-		  $page = 1;
-								
-		//執行SQL查詢
-		$sql = "SELECT id, author, tag, subject, content, date FROM systemupdate ORDER BY date DESC";
-		$result = execute_sql($link, "news", $sql);
-							
-		//取得記錄數 
-		$total_records = mysqli_num_rows($result);
-						
-		//計算總頁數
-		$total_pages = ceil($total_records / $records_per_page);
-						
-		//計算本頁第一筆記錄的序號
-		$started_record = $records_per_page * ($page - 1);
-						
-		//將記錄指標移至本頁第一筆記錄的序號
-		mysqli_data_seek($result, $started_record);
-		
-		$j = 1;
-		
-		echo"<div class='row'>";
-			while ($row = mysqli_fetch_assoc($result) and $j <= $records_per_page)
-			{				
-				echo"<div class='center-align'>";
-					echo"<div class='col s4 m4'>";
-						echo"<div class='card blue-grey darken-1'>";
-							echo"<div class='card-content white-text'>";
-								echo"<span class='card-title'>".$row["subject"]."</span>";
-								echo"<br>";
-								echo"<p class='chip brown left'>".$row["tag"]."</p>";
-								echo"<br><br>";
-								echo"<h5 class=''>".$row["content"]."</h5>";
-								echo"<br>";
-								echo"<span class='author right'>發文者:".$row["author"]."</span>";
-								echo"<br>";
-								echo"<p class='right'>更新時間:".$row["date"]."</p>";
-							echo"</div>";
-							echo"<br>";
-						echo"</div>";
-					echo"</div>";
-				echo"</div>";
-				
-				$j++;
-			}
-		echo"</div>";
-		
-		//產生導覽列
-		echo"<ul class='pagination center'>";
-					
-		if ($page > 1)
-					echo "<li class='waves-effect'><a href='main.php?page=". ($page - 1) . "'><i class='material-icons'>chevron_left</i></a></li>";
-							
-		for ($i = 1; $i <= $total_pages; $i++)
-		{
-		  if ($i == $page)
-			echo "<li class='waves-effect'><a href='main.php?page=$i'>$i</a></li>";
-		  else
-			echo"<li class='waves-effect'><a href='main.php?page=$i'>$i</a></li>";
-		}
-						
-		if ($page < $total_pages)
-					echo"<li class='waves-effect'><a href='review.php?page=". ($page + 1) . "'><i class='material-icons'>chevron_right</i></a></li>";
-		echo "</p>";
-		
-		echo"</ul>";
-		
-	?>
-</div>	
-
-	<section id="slogan" class="tm-section-pad-top">
-		<div class="row">
-		  <div class="center-align col-12">
-			<br>
-			<h2 class="center-align">NHUSH CITY</h2>
-			<p class="center-align">
-			你能想像一個專屬於我們的社區嗎? 你可以在這裡分享對於學校與學習相關的任何問題。
-			<br>
-			<br>
-			我們會不斷精進此系統，請提供任何意見給開發團隊。
-			</p>
-			<br>			
-			<br>
-		  </div>
-		</div>		  
-	</section>
-	
-	<?php
-	
-		//執行SQL查詢
-		$sql = "SELECT id,billboard,date FROM bulletin_board ORDER BY date DESC";
-		$result = execute_sql($link, "news", $sql);
-		$row = mysqli_fetch_assoc($result);
-		
-	?>
-	
-	<div class="container">
-		<section id="plan" class="tm-section-pad-top">
-			  <div class="container">
+	Vue.component('contact', {
+	  template:  
+		`<div class="container">
+			<div class="container">
 				<div class="center-align">
-					<h3 class="tm-text-primary tm-section-title mb-4">公佈欄</h3>
-				</div>
-				  <div class="col s6 m6">
-					<div class="card horizontal small">
-					  <div class="card-stacked">
-						<div class="card-content">
-							<h4>
-								<blockquote>
-									<?php  echo $row["billboard"]  ?>
-								</blockquote>
-							</h4>
-						</div>
-					  </div>
-					</div>
-				  </div>
-		</section>
-	</div>
-		
-	<br><br>
-	
-	<div class="container">
-		<section id="plan" class="tm-section-pad-top">
-		      <div class="container">
-		        <div class="center-align">
-					<h3 class="tm-text-primary tm-section-title mb-4">CONTACT</h3>
-		        </div>		
-		        <div class="col s6 m6">
+					<h3>CONTACT</h3>
+				</div>		
+				<div class="col s6 m6">
 					<div class="card horizontal small">
 						<div class="card-stacked">
 							<div class="card-content">
 								<h4>
-								<blockquote><i class="material-icons medium">perm_phone_msg</i>02-26308889</p>
+								<blockquote>
+								<i class="material-icons medium">perm_phone_msg</i>02-26308889</p>
 								<i class="material-icons medium">contacts</i>臺北市內湖區康寧路3段220號</p>
 								</blockquote>
 							</div>
 						</div>
-		            </div>
-		        </div>
-		</section>
-	</div>
+					</div>
+				</div>
+			</div>
+		</div>`
+	})
 	
-	<br><br>
+	Vue.component('footers', {
+	  template:  
+		`<footer class="page-footer brown">
+			<div class="container">
+			  <div class="row">
+				<div class="col l6 s12">
+				  <h5 class="white-text">南湖資訊社</h5>
+				  <p class="grey-text text-lighten-4">We are students of Nanhu High School and we love Computer Science and Information Engineering.</p>
+				  <p class="grey-text text-lighten-4">This website is completed by our students and teachers.</p>
+				</div>
+				<div class="col l3 s12">
+				  <h5 class="white-text">相關連結</h5>
+				  <ul>
+					<li><a class="white-text" href="http://www.nhush.tp.edu.tw/default_page.asp">南湖高中官網</a></li>
+					<li><a class="white-text" href="https://e-portfolio.cooc.tp.edu.tw/Portal.do">臺北市學習歷程檔案系統</a></li>
+					<li><a class="white-text" href="https://sschool.tp.edu.tw/Login.action?schNo=403303">台北市高中第二代校務行政系統</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</div>
+			<div class="footer-copyright">
+			  <div class="container">
+				<p class="center-align">Made by <a class="orange-text text-lighten-3" href="http://www.materializecss.cn">Materialize</a></p>
+			  </div>
+			</div>
+		</footer>`
+	})
 	
-</div>
-
-<footer class="page-footer brown">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">南湖資訊社</h5>
-          <p class="grey-text text-lighten-4">We are students of Nanhu High School and we love Computer Science and Information Engineering.</p>
-		  <p class="grey-text text-lighten-4">This website is completed by our students and teachers.</p>
-        </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">相關連結</h5>
-          <ul>
-            <li><a class="white-text" href="http://www.nhush.tp.edu.tw/default_page.asp">南湖高中官網</a></li>
-            <li><a class="white-text" href="https://e-portfolio.cooc.tp.edu.tw/Portal.do">臺北市學習歷程檔案系統</a></li>
-            <li><a class="white-text" href="https://sschool.tp.edu.tw/Login.action?schNo=403303">台北市高中第二代校務行政系統</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-		<p class="center-align">Made by <a class="orange-text text-lighten-3" href="http://www.materializecss.cn">Materialize</a></p>
-      </div>
-    </div>
-</footer>
-
-
-	<!--  Scripts-->
-	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script src="js/materialize.js"></script>
-	<script src="js/init.js"></script>
-	<script type="text/javascript">
+	new Vue({
+	    el: '#app',
+	    data: {
+	        fontsizesetting: 30,
+	        fontStyle: {
+	            'font-size': '30px'
+	        }
+	    }
+	})
 	
 	$(document).ready(function(){
 		$('.parallax').parallax();
@@ -434,7 +476,6 @@
 		$(".button-collapse").sideNav();
 	});
 	
-	</script>
-	
+</script>
   </body>
 </html>
